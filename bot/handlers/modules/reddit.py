1a28f29a0fa3173a53de17af4816a2079b667948
not-ayan <notayan69@gmail.com>
2025-01-31 15:35:38 +0530

    Remove praw

--- bot/handlers/modules/reddit.py
+++ bot/handlers/modules/reddit.py
@@ -1,5 +1,4 @@
 import time
-import praw
 import yt_dlp
 import os
 import logging
@@ -13,25 +12,8 @@ router = Router()
 # Load environment variables
 load_dotenv()
 
-# Initialize PRAW with your Reddit app credentials
-reddit_client_id = os.getenv('REDDIT_CLIENT_ID')
-reddit_client_secret = os.getenv('REDDIT_CLIENT_SECRET')
-reddit_user_agent = os.getenv('REDDIT_USER_AGENT')
-
-logging.info(f"Reddit client ID: {reddit_client_id}")
-logging.info(f"Reddit client secret: {reddit_client_secret}")
-logging.info(f"Reddit user agent: {reddit_user_agent}")
-
-reddit = praw.Reddit(
-    client_id=reddit_client_id,
-    client_secret=reddit_client_secret,
-    user_agent=reddit_user_agent
-)
-
 def download_reddit_post(url: str, filename: str) -> str:
     logging.info(f"Downloading Reddit post from URL: {url}")
-    submission = reddit.submission(url=url)
-    media_url = submission.url if not submission.is_video else submission.media['reddit_video']['fallback_url']
     
     ydl_opts = {
         'outtmpl': filename,
@@ -39,7 +21,7 @@ def download_reddit_post(url: str, filename: str) -> str:
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([media_url])
+        ydl.download([url])
     
     return filename
 
