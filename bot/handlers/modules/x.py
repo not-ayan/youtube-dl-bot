80c3a4c3e1842b9448040d7e5cb04ed041f0e3ae
not-ayan <notayan69@gmail.com>
2025-02-01 13:54:10 +0530

    remove vidcount

--- bot/handlers/modules/x.py
+++ bot/handlers/modules/x.py
@@ -50,29 +50,16 @@ links = [
     "https://twitter.com/",
 ]
 
-def keyboard(number: int, url: str) -> types.InlineKeyboardMarkup:
-    kb = [[types.InlineKeyboardButton(text=f"Video {i+1}", callback_data=f"{url}!{i}")] for i in range(number)]
-    return types.InlineKeyboardMarkup(inline_keyboard=kb)
-
 @router.message(F.text.startswith(tuple(links)))
 async def x(message: types.Message) -> None:
     mention = f'<a href="tg://user?id={message.from_user.id}">{message.from_user.full_name}</a>'
-    count = vids_count(message.text)
-
-    if count > 1:
-        try:
-            await message.delete()
-        except exceptions.TelegramBadRequest:
-            pass
-        await message.answer("Multiple videos found in the post. Please select which one you want to download", reply_markup=keyboard(count, message.text))
-    else:
-        filename = f"{time.time_ns()}-{message.from_user.id}.mp4"
-        await master_handler(
-            message=message,
-            send_function=message.answer_video,
-            download_function=lambda: download_x(message.text, filename),
-            caption=f'<a href="{message.text}">Source</a>\nShared by {mention}'
-        )
+    filename = f"{time.time_ns()}-{message.from_user.id}.mp4"
+    await master_handler(
+        message=message,
+        send_function=message.answer_video,
+        download_function=lambda: download_x(message.text, filename),
+        caption=f'<a href="{message.text}">Source</a>\n\nUploaded by {mention}'
+    )
 
 @router.callback_query(lambda c: c.data.startswith(tuple(links)))
 async def x2(callback: types.CallbackQuery) -> None:
@@ -83,6 +70,6 @@ async def x2(callback: types.CallbackQuery) -> None:
     await master_handler(
         message=callback.message,
         send_function=callback.message.answer_video,
-        download_function=lambda: download_x(data[0], filename, int(data[-1])),
-        caption=f'<a href="{data[0]}">Source</a>\nShared by {mention}'
+        download_function=lambda: download_x(data[0], filename),
+        caption=f'<a href="{data[0]}">Source</a>\nUploaded by {mention}'
     )
